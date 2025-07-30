0fa0990c7c4da3b568fdfae8987ddc795e785cfb
Jack Duignan <56944950+jaxsonpd@users.noreply.github.com>
2025-07-30 15:44:35 +1200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
 -- [[ init.lua ]]
 -- Author: Jack Duignan (JackpDuignan@gmail.com)
 
--- LEADER
+-- [[ LEADER ]]
 vim.g.mapleader = ","
 vim.g.localleader = "\\"
 
-
+-- [[ Update packages on changes to plug.lua ]]
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plug.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 
 -- [[ Modules ]]
